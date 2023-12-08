d626d35525a2debf9761257e31624c643667063f
Michal Natonek <mnat@thaumatec.com>
2023-12-08 23:24:04 +0100

    Initial project creation

---
+++ bin/day_01.ml
@@ -0,0 +1,6 @@
+let input = Aoc2023.read_lines "day_1.input"
+(* let () = *)
+(*   match Aoc2023.is_digit '9' with *)
+(*   | true -> print_endline "true" *)
+(*   | false -> print_endline "false" *)
+(* ;; *)
