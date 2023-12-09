a1090109d1757fd708b8ba194d425de5b4df80b2
Michal Natonek <mnat@thaumatec.com>
2023-12-09 21:21:13 +0100

    Day 2 part 2 solution

--- bin/day_02.ml
+++ bin/day_02.ml
@@ -26,3 +26,20 @@ let () =
   |> List.fold ~init:0 ~f:( + )
   |> Format.printf "Part 1: %d@."
 ;;
+
+let () =
+  let input = Aoc2023.read_lines "./inputs/day_02.input" in
+  let find_max line (_, re) =
+    let matches = Re.all re line in
+    List.fold matches ~init:0 ~f:(fun acc m ->
+      match acc with
+      | 0 -> Re.Group.get m 1 |> Int.of_string
+      | x when x < (Re.Group.get m 1 |> Int.of_string) ->
+        Re.Group.get m 1 |> Int.of_string
+      | _ -> acc)
+  in
+  List.map input ~f:(fun line ->
+    List.fold constrains ~init:1 ~f:(fun acc con -> find_max line con * acc))
+  |> List.fold ~init:0 ~f:( + )
+  |> Format.printf "Part 2: %d@."
+;;
